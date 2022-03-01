2ab43ff2f6ce76ae3d5391182df81c4406ef0ea7
Benjamin Wagner <wagnerb4@students.uni-marburg.de>
2022-03-01 20:40:55 +0100

    my solution

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,7 +4,6 @@ use rand::Rng;
 
 fn main() {
 	
-    let mut user_input = String::new();
 	let console_in = io::stdin();
 	let mut rng = rand::thread_rng();
 	
@@ -14,27 +13,35 @@ fn main() {
 	println!("Your random number is {}.", random_number);
 	
     loop {
+        println!("Please enter your guess.");
+		let mut user_input = String::new();
 		console_in
 			.read_line(&mut user_input)
 			.expect("Your input couldn't be read.");
 		
-		let user_result = user_input.trim().parse(); 
+		println!("Your input was: {}", user_input);
+		let mut user_input_parsed: i32 = -1; 
 		
-		let user_input: i32 = match user_input.trim().parse() {
-			io::Result::Ok(user_input) => user_input,
-			io::Result::Err(_) => println!("Please enter a number. You entered '{}'.", user_input)
+		match user_input.trim().parse() {
+            Result::Ok(value) => {
+                if value < 1 || value > 100 {
+                    println!("Pleas enter a number between 1 and 100.");
+                }
+                user_input_parsed = value;
+            },
+            Result::Err(_) => println!("Please enter a number. You entered '{}'.", user_input)
 		};
 		
-		println!("Your input was: {}", user_input);
-		
-		match user_input.cmp(&random_number) {
-			Ordering::Less => println!("enter a higher number"),
-			Ordering::Greater => println!("enter a smaller number"),
-			Ordering::Equal => {
-				println!("You win. Congratulations!");
-				break;
-			}
-		}		
+		if user_input_parsed != -1 {
+            match user_input_parsed.cmp(&random_number) {
+                Ordering::Less => println!("enter a higher number"),
+                Ordering::Greater => println!("enter a smaller number"),
+                Ordering::Equal => {
+                    println!("You win. Congratulations!");
+                    break;
+                }
+            }
+		}
 	}
 	
 }
