7775c4fac25a8762f607d27085336b1ce99ca9fa
Benjamin Wagner <wagnerb4@students.uni-marburg.de>
2022-03-01 20:51:06 +0100

    used continue keyword and u32 number type

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,47 +1,46 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-	
-	let console_in = io::stdin();
-	let mut rng = rand::thread_rng();
-	
-	println!("Welcome to the guessing game.");
-	
-	let random_number: i32 = rng.gen_range(1..=100);
-	println!("Your random number is {}.", random_number);
-	
+    let console_in = io::stdin();
+    let mut rng = rand::thread_rng();
+
+    println!("Welcome to the guessing game.");
+
+    let random_number: u32 = rng.gen_range(1..=100);
+    println!("Your random number is {}.", random_number);
+
     loop {
         println!("Please enter your guess.");
-		let mut user_input = String::new();
-		console_in
-			.read_line(&mut user_input)
-			.expect("Your input couldn't be read.");
-		
-		println!("Your input was: {}", user_input);
-		let mut user_input_parsed: i32 = -1; 
-		
-		match user_input.trim().parse() {
+        let mut user_input = String::new();
+        console_in
+            .read_line(&mut user_input)
+            .expect("Your input couldn't be read.");
+
+        println!("Your input was: {}", user_input);
+        let user_input_parsed: u32;
+
+        match user_input.trim().parse() {
             Result::Ok(value) => {
                 if value < 1 || value > 100 {
                     println!("Pleas enter a number between 1 and 100.");
                 }
                 user_input_parsed = value;
-            },
-            Result::Err(_) => println!("Please enter a number. You entered '{}'.", user_input)
-		};
-		
-		if user_input_parsed != -1 {
-            match user_input_parsed.cmp(&random_number) {
-                Ordering::Less => println!("enter a higher number"),
-                Ordering::Greater => println!("enter a smaller number"),
-                Ordering::Equal => {
-                    println!("You win. Congratulations!");
-                    break;
-                }
             }
-		}
-	}
-	
+            Result::Err(_) => {
+                println!("Please enter a number. You entered '{}'.", user_input);
+                continue;
+            }
+        };
+
+        match user_input_parsed.cmp(&random_number) {
+            Ordering::Less => println!("enter a higher number"),
+            Ordering::Greater => println!("enter a smaller number"),
+            Ordering::Equal => {
+                println!("You win. Congratulations!");
+                break;
+            }
+        }
+    }
 }
